8043ff5f34465890ead733d3128a2d7234b5de65
nikita <nbarabanschikov@mts.ru>
2024-03-09 20:55:13 +0300

    Xin chào

--- telegramm_bot/bot.py
+++ telegramm_bot/bot.py
@@ -3,6 +3,43 @@ from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandl
 import os
 
 
+
+
+
+# Define a command handler. These usually take the two arguments update and context.
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """Send a message when the command /start is issued."""
+    await update.message.reply_text("Xin chào!")
+
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Echo the user message."""
+    await update.message.reply_text(update.message.text)
+
+
+# Create the Application and pass it your bot's token.
 application = Application.builder().token("TOKEN").build()
+
+# on different commands - answer in Telegram
 application.add_handler(CommandHandler("start", start))
+
+# on non command i.e. message - echo the message on Telegram
+application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+
+# Run the bot until the user presses Ctrl-C
 application.run_polling(allowed_updates=Update.ALL_TYPES)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
