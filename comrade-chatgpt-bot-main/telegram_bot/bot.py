c4eb07aa4b20537b53a3f7734fdc1c7d6c98b064
nikita <nbarabanschikov@mts.ru>
2024-03-12 13:59:40 +0300

    add generate image

--- comrade-chatgpt-bot-main/telegram_bot/bot.py
+++ comrade-chatgpt-bot-main/telegram_bot/bot.py
@@ -6,18 +6,21 @@ from openai import OpenAI
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 from telegram import ForceReply, Update
-from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    ContextTypes,
+    MessageHandler,
+    filters,
+)
 
 
 # Enable logging
 logging.basicConfig(
     format='timestamp=%(asctime)s logger=%(name)s level=%(levelname)s msg="%(message)s"',
-    datefmt='%Y-%m-%dT%H:%M:%S',
+    datefmt="%Y-%m-%dT%H:%M:%S",
     level=logging.INFO,
-    handlers=[
-        logging.FileHandler("./logs/bot.log"),
-        logging.StreamHandler()
-    ]
+    handlers=[logging.FileHandler("./logs/bot.log"), logging.StreamHandler()],
 )
 # set higher logging level for httpx to avoid all GET and POST requests being logged
 logging.getLogger("httpx").setLevel(logging.WARNING)
@@ -45,33 +48,40 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
     await update.message.reply_text("Help!")
 
 
+async def generate_image(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Generate image when the command /image is issued"""
+    response_image = client.images.generate(
+        model="dall-e-3",
+        prompt="Vietnamese resident",
+        size="1024x1024",
+        quality="standard",
+         n=1,
+    )
+    image_url = response_image.data[0].url    
+    await update.message.reply_text(image_url)
+
+
 # 2024-02-28T11:48:14.892862627Z ChatCompletion(id='chatcmpl-8xChybGg2uRWPk0hagGRIdHvjoaAX', choices=[Choice(finish_reason='stop', index=0, logprobs=None, message=ChatCompletionMessage(content='Hello! How can I assist you today?', role='assistant', function_call=None, tool_calls=None))], created=1709120894, model='gpt-3.5-turbo-0125', object='chat.completion', system_fingerprint='fp_86156a94a0', usage=CompletionUsage(completion_tokens=9, prompt_tokens=18, total_tokens=27))
 async def gpt_prompt(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user_message = update.message.text
-    
+
     logger.info("Пользователь написал сообщение {0}".format(user_message))
     try:
         response = client.chat.completions.create(
             model="gpt-3.5-turbo",
             messages=[
                 {"role": "system", "content": "You are comrade ho chi minh"},
-                {"role": "user", "content": user_message}
-            ]
+                {"role": "user", "content": user_message},
+            ],
         )
         # Access text content from "message" within the first "Choice"
         ai_response = response.choices[0].message.content
         await update.message.reply_text(ai_response.strip())
     except Exception as e:
         logger.error(f"Error generating AI response: {e}")
-        await update.message.reply_text("Sorry, I couldn't process your message at the moment.")
-        
-        
-        
-
-        
-        
-        
-    
+        await update.message.reply_text(
+            "Sorry, I couldn't process your message at the moment."
+        )
 
 
 def main() -> None:
@@ -83,7 +93,7 @@ def main() -> None:
     # on different commands - answer in Telegram
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help_command))
-    
+
     application.add_handler(CommandHandler("image", generate_image))
 
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, gpt_prompt))
