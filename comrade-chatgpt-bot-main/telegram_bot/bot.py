47f2d27face38ca4e808394ca3f31701ae69b591
nikita <nbarabanschikov@mts.ru>
2024-03-27 09:56:30 +0300

    добавил базу и новые функции

--- comrade-chatgpt-bot-main/telegram_bot/bot.py
+++ comrade-chatgpt-bot-main/telegram_bot/bot.py
@@ -167,20 +167,20 @@ async def disable_user(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
         await update.message.reply_text(f"User {user_id_to_disable} access revoked.")
     finally:
         await conn.close()
-        
+
+
 async def log_allowed_users(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     conn = await db_connect()
     try:
         users = await conn.fetch("SELECT user_id FROM allowed_users ORDER BY user_id")
         if users:
-            user_ids = ', '.join([str(user['user_id']) for user in users])
+            user_ids = ", ".join([str(user["user_id"]) for user in users])
             logger.info(f"Allowed users: {user_ids}")
         else:
             logger.info("No users are currently allowed.")
         await update.message.reply_text("Logging allowed users...")
     finally:
         await conn.close()
-
 
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -192,45 +192,49 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
 async def generate_image(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user_message = update.message.text
     user = update.effective_user
-    
+
     if not await is_user_allowed(user.id):
-        logger.info(f"User {user.id} ({user.username}) tried to generate an image but is not allowed.")
-        await update.message.reply_text("Sorry, you are not allowed to generate images.")
+        logger.info(
+            f"User {user.id} ({user.username}) tried to generate an image but is not allowed."
+        )
+        await update.message.reply_text(
+            "Sorry, you are not allowed to generate images."
+        )
         return
-    
+
     if not context.args:
-        logger.error(f"User {user.id} ({user.username}) did not provide a prompt for the /image command.")
-        await update.message.reply_text("Please provide a description for the image after the /image command.")
+        logger.error(
+            f"User {user.id} ({user.username}) did not provide a prompt for the /image command."
+        )
+        await update.message.reply_text(
+            "Please provide a description for the image after the /image command."
+        )
         return
 
     prompt = " ".join(
         context.args
     )  # принимать в качестве promзt аргументы отпользователя
     logging.info(f"User {user.id} ({user.username}) requested to generate image")
-    
-    # async def keep_upload_photo():
-    #     while keep_upload_photo.is_upload_photo:
-    #         await context.bot.send_chat_action(chat_id=update.effective_chat.id, action='upload_photo')
-    #         await asyncio.sleep(5)
-    
-    # keep_upload_photo.is_upload_photo = True
 
-    # upload_photo_task = asyncio.create_task(keep_upload_photo())        
-            
-    # try:        
-    """Generate image when the command /image is issued"""
-    response_image = client.images.generate(
-        model="dall-e-3",
-        prompt=prompt,
-        size="1024x1024",
-        quality="standard",
-        n=1,
-    )
-    image_url = response_image.data[0].url
-    responce_url = requests.get(image_url)
-    with open("./images/image.png", "wb") as f:
-        f.write(responce_url.content)
-    await update.message.reply_photo(open("./images/image.png", "rb"))
+    try:
+        """Generate image when the command /image is issued"""
+        response_image = client.images.generate(
+            model="dall-e-3",
+            prompt=prompt,
+            size="1024x1024",
+            quality="standard",
+            n=1,
+        )
+        image_url = response_image.data[0].url
+        responce_url = requests.get(image_url)
+        with open("./images/image.png", "wb") as f:
+            f.write(responce_url.content)
+        await update.message.reply_photo(open("./images/image.png", "rb"))
+    except Exception as e:
+        logging.error(f"Error generating image for prompt: '{prompt}': {e}")
+        await update.message.reply_text(
+            "Sorry, there was an error generating your image."
+        )
 
     # 2024-02-28T11:48:14.892862627Z ChatCompletion(id='chatcmpl-8xChybGg2uRWPk0hagGRIdHvjoaAX', choices=[Choice(finish_reason='stop', index=0, logprobs=None, message=ChatCompletionMessage(content='Hello! How can I assist you today?', role='assistant', function_call=None, tool_calls=None))], created=1709120894, model='gpt-3.5-turbo-0125', object='chat.completion', system_fingerprint='fp_86156a94a0', usage=CompletionUsage(completion_tokens=9, prompt_tokens=18, total_tokens=27))
 
@@ -238,6 +242,13 @@ async def generate_image(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
 async def gpt_prompt(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user_message = update.message.text
     user = update.effective_user
+
+    if not await is_user_allowed(user.id):
+        logger.info(
+            f"User {user.id} ({user.username}) tried to use GPT prompt but is not allowed."
+        )
+        await update.message.reply_text("Sorry, you are not allowed to text with me.")
+        return
 
     # logger.info("Пользователь написал сообщение {0}".format(user_message))
 
