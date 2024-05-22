9f7a9ab10fda851641ccb490dd39ab330c5e9901
nikita <nbarabanschikov@mts.ru>
2024-05-22 12:46:58 +0300

    add unit test

--- comrade-chatgpt-bot-main/telegram_bot/test-bot.py
+++ comrade-chatgpt-bot-main/telegram_bot/test-bot.py
@@ -1,12 +1,17 @@
 import unittest
-# from bot import main
 import os
 
+class TestFileSize(unittest.TestCase):  # предоставляем класс TestCase для создания тестовых случаев
+
+    def test_file_size(self):
+        file_path = "./restore.yaml"
+        file_size = os.stat(file_path)
+
+        size_in_mb = file_size.st_size / (1024 * 1024)
+
+        # проверка, что размер файла больше 1 МБ
+        self.assertTrue(size_in_mb > 1, "File size is less than 1 MB")# проверяем условие на истинну
 
 
-# def test_main()
-
-token_for_check = os.getenv("TELEGRAM_BOT_TOKEN")
-
-print (token_for_check)
-
+if __name__ == '__main__': #запускаем тест,эквиталентно команде python3 -m  unittest test-bot.py
+    unittest.main()
