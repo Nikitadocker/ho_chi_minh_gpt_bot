014dbf563f24349ece3b422879de46a0835e85c1
nikita <nbarabanschikov@mts.ru>
2024-06-01 14:22:12 +0300

    add cert

--- comrade-chatgpt-bot-main/digitalocean/helm_cert_manager/main.tf
+++ comrade-chatgpt-bot-main/digitalocean/helm_cert_manager/main.tf
@@ -1,14 +1,27 @@
-terraform {
-  required_providers {
-    helm = {
-      source = "hashicorp/helm"
-      version = "2.13.2"
-    }
-  }
+provider "kubectl" {
+  config_path= "~/.kube/config_india"
 }
-
 provider "helm" {
   kubernetes {
+    config_path= "~/.kube/config_india"
+  }
+}
+provider "kubernetes" {
   config_path= "~/.kube/config_india"
+}
+terraform {
+  required_providers {
+    kubectl = {
+      source  = "alekc/kubectl"
+      version = ">= 2.0.2"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.5.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.0.1"
+    }
   }
 }
