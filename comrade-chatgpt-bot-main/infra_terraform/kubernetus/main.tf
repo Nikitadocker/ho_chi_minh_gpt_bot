2acaa78e5188b2a8cb485442bad9517173cfb1f3
nikita <nbarabanschikov@mts.ru>
2024-06-07 22:28:43 +0300

    add

--- comrade-chatgpt-bot-main/infra_terraform/kubernetus/main.tf
+++ comrade-chatgpt-bot-main/infra_terraform/kubernetus/main.tf
@@ -5,6 +5,18 @@ terraform {
       version = "~> 2.0"
     }
   }
+
+  backend "s3" {
+    endpoint                    = "projectbucket.blr1.digitaloceanspaces.com"
+    key                         = "terraform.tfstate"
+    bucket                      = "projectbucket"
+    region                      = "nyc3"
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+   
+  }
+
+
 }
 
 
@@ -13,3 +25,6 @@ terraform {
 provider "digitalocean" {
   token = var.do_token
 }
+
+
+
