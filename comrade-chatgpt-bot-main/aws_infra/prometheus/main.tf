e09a63fb599b495b01b8f6ec39d885a76f78df17
nikita <nbarabanschikov@mts.ru>
2024-06-23 20:36:01 +0300

    add pv

---
+++ comrade-chatgpt-bot-main/aws_infra/prometheus/main.tf
@@ -0,0 +1,57 @@
+terraform {
+  required_version = ">= 1.4.7"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.53.0"
+    }
+
+    kubectl = {
+      source  = "alekc/kubectl"
+      version = ">= 2.0.2"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.5.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.0.1"
+    }
+  }
+
+  backend "s3" {
+    profile        = "study"
+    bucket         = "study-terraform-states"
+    key            = "prometheus.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+  }
+}
+
+provider "aws" {
+  profile = "study"
+  region  = "us-east-1"
+}
+
+
+provider "kubectl" {
+  config_path= "~/.kube/config"
+  config_context = "arn:aws:eks:us-east-1:632497825040:cluster/study-cluster"
+
+}
+
+provider "helm" {
+  kubernetes {
+    config_path= "~/.kube/config"
+    config_context = "arn:aws:eks:us-east-1:632497825040:cluster/study-cluster"
+
+  }
+}
+
+provider "kubernetes" {
+  config_path= "~/.kube/config"
+  config_context = "arn:aws:eks:us-east-1:632497825040:cluster/study-cluster"
+
+}
