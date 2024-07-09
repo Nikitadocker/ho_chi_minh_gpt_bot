0938118fc467b653ce3d723cbbb478dd0e2765f5
nikita <nbarabanschikov@mts.ru>
2024-07-09 20:27:07 +0300

    add logger for healthceck

--- comrade-chatgpt-bot-main/telegram_bot/bot.py
+++ comrade-chatgpt-bot-main/telegram_bot/bot.py
@@ -90,6 +90,10 @@ def healthcheck():
     """Check the health of the bot's dependencies."""
     # openai_ok = check_openai_connection()
     openai_ok = True
+    
+    healthcheck_log = logging.getLogger('werkzeug')
+    healthcheck_log.setLevel(logging.ERROR)
+
 
     status = "OK" if openai_ok else "ERROR"
     return jsonify({"status": status}), 200 if status == "OK" else 500
