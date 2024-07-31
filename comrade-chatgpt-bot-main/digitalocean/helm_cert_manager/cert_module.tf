bf192a52447aadb791b6a95fee529453f4bec836
nikita <nbarabanschikov@mts.ru>
2024-07-31 13:33:07 +0300

    delete do terraform

--- comrade-chatgpt-bot-main/digitalocean/helm_cert_manager/cert_module.tf
+++
@@ -1,9 +0,0 @@
-module "cert_manager" {
-  source  = "terraform-iaac/cert-manager/kubernetes"
-  version = "2.6.3"
-  cluster_issuer_email                   = "neketa51993@gmail.com"
-  cluster_issuer_name                    = "letsencrypt-prod"
-  cluster_issuer_private_key_secret_name = "cert-manager-private-key"
-  namespace_name = "cert-manager"
-  create_namespace	= "true"
-}
